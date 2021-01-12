5831aa4a6d119068c83db2e3434496d487c0ffe2
Wesley Adriann <wesleyadriann@gmail.com>
2021-01-12 19:17:43 -0300

    Adds to list rooms and create fixed room
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import firebase from './services/firebase';
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+  // const [room, setRoom] = useState('')
+  // const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    let roomsRef = null;
+
+    firebase.db.once('value')
+      .then((snap) => {
+        if(!snap.child('rooms').exists()) createRoom('main');
+        const roomsChild = snap.child('rooms');
+ 
+        roomsChild.ref.once('value')
+          .then((snapshot) => {
+
+            let remoteRooms = [];
+            if(snapshot.val()) 
+              remoteRooms = Object.entries(snapshot.val()).map(([id, name]) => ({id, name}));
+            
+            setRooms(remoteRooms);
+          })
+        
+        roomsRef = roomsChild.ref;
+        
+        // roomsRef.on('value', (snapshot) => {
+        //   console.log(snapshot.val())
+        // })
+
+        roomsRef.on('child_added', (snapshot) => {
+          setRooms((prevRooms) => [...prevRooms, {id: snapshot.key, name: snapshot.val()}]);
+        })
+
+        roomsRef.on('child_removed', (snapshot) => {
+          console.log(snapshot.val());
+        })
+
+        roomsRef.on('child_changed', (snapshot) => {
+          console.log(snapshot.val());
+        })
+      })
+
+    return roomsRef?.off
+  }, [])
+
+  const createRoom = (roomName) => {
+    const roomRef = firebase.db.child('rooms').push(roomName);
+    firebase.db.child('roomsMessages').child(roomRef.key).push({user: 'admin', message: 'initial'});
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => createRoom('teste')}>Create</button>
+      {
+        rooms.map((room) => (
+          <p key={room.id}>{room.name}</p>
+        ))
+      }
     </div>
   );
 }
